46f103707fd69c2d6662c1dd0258b3d6fe2b73a1
Nandini Dusane <64679253+nandinidusane@users.noreply.github.com>
2023-05-24 10:35:01 +0530

    Metamask as a Payment Gateway

--- script.js
+++ script.js
@@ -9,22 +9,87 @@ function addToCart(productId, productName, price) {
 function updateCart() {
   const cartList = document.getElementById('cart-items');
   cartList.innerHTML = '';
-  
-  for (let i = 0; i < cartItems.length; i++) {
-    const cartItem = cartItems[i];
+
+  cartItems.forEach((cartItem) => {
     const listItem = document.createElement('li');
-    listItem.innerHTML = cartItem.name + ' - Rs' + cartItem.price;
+    listItem.textContent = cartItem.name + ' - ETH' + cartItem.price;
     cartList.appendChild(listItem);
+  });
+}
+
+function pay()
+{
+  if (typeof window.ethereum === 'undefined') {
+    alert('Please install Metamask to proceed with the payment.');
+    return;
+  }
+
+  ethereum.request({ method: 'eth_requestAccounts' })
+    .then(handleAccounts)
+    .catch(handleError);
+    // ethereum.request function is part of the Ethereum JavaScript API . function takes an object as its argument, specifying the method and parameters for the request
+
+}
+
+function handleAccounts(accounts) {
+  const userAddress = accounts[0];
+  const paymentAmount = calculatePaymentAmount();
+
+  ethereum.request({
+    method: 'eth_sendTransaction',
+    params: [
+      {
+        from: userAddress,
+        to: '0x3Fe9dC4107f1B25DB7d6609454182dDdc185efF7',
+        value: paymentAmount,
+      },
+    ],
+  })
+    .then(handleTransactionSuccess)
+    .catch(handleError);
+}
+
+function handleTransactionSuccess(txHash) {
+  alert('Payment successful!\nTransaction Hash: ' + txHash);
+  removePurchasedProducts();
+  // displays an alert with the payment success message and the transaction hash. Then it calls the removePurchasedProducts() function to remove the purchased products from the cart and the website.
+}
+
+function handleError(error) {
+  alert('Payment failed or an error occurred. Please try again.');
+  console.error(error);
+}
+
+function calculatePaymentAmount() {
+  let totalAmount = 0;
+
+  cartItems.forEach((cartItem) => {
+    totalAmount += cartItem.price;
+  });
+
+  const paymentAmount = totalAmount * 1e18;
+
+  return paymentAmount.toString();
+}
+
+function removePurchasedProducts() {
+  const purchasedProducts = [...cartItems];
+  cartItems = [];
+  updateCart();
+
+  purchasedProducts.forEach((cartItem) => {
+    removeProductFromWebsite(cartItem.id);
+  });
+}
+
+function removeProductFromWebsite(productId) {
+  const productElement = document.getElementById(productId);
+  if (productElement) {
+    productElement.remove();
   }
 }
 
-function pay() {
-  for (let i = 0; i < cartItems.length; i++) {
-    const cartItem = cartItems[i];
-    const productElement = document.getElementById(cartItem.id);
-    productElement.style.display = 'none';
-  }
-  
+function clearCart() {
   cartItems = [];
   updateCart();
 }
